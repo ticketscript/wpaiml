f686250f0e47e2372de9987f12b4aa5bc0affc47
Niels Harland <niels.harland@ticketscript.nl>
2015-04-14 12:11:40 +0200

    Initital commit

---
+++ wp-all-import-multi-lang.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP All Import Multi Lang
+Description: Add-on to migrate existing multi language content to WPML
+Version: 0.9
+Author: Niels Harland @ ticketscript b.v.
+*/
+
+
+include "rapid-addon.php";
+
+$wpai_wpml = new RapidAddon('WP All Import Multi Lang', 'wpai_wpml');
